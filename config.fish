0ef278f0f9909f9affe8156575c041436e2afae1
Yasuo Omori <yasuo@YasuonoMacBook-Pro.local>
2020-08-01 04:55:00 +0900

    add fish plugin-balias

--- config.fish
+++ config.fish
@@ -1,2 +1,8 @@
+# fzf
 set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+
+# alias, balias
+balias gst 'git status' 
+balias gf 'git fetch'
+balias gm 'git merge'
